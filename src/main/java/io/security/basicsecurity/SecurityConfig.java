fad7bbdf2f522a058f102a8ae311fc42609cd21b
howisitgoing <howisitgoing@kakao.com>
2022-12-17 18:19:16 +0900

    스프링 시큐리티  - 스프링 시큐리티 기본 API 및 Filter 이해   - 동시 세션 제어, 세션 고정 보호, 세션 정책

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,14 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -27,21 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final LoginSuccessHandler loginSuccessHandler;
     private final LoginFailureHandler loginFailureHandler;
-
-
-    /**
-     * <pre>
-     *     http.formLogin()
-     *             .loginPage("/login.html") // 사용자 정의 로그인 페이지
-     *             .defaultSuccessUrl("/home") // 로그인 성공 후 이동 페이지
-     *             .failureUrl("/login.html?error=true") // 로그인 실패 후 이동 페이지
-     *             .usernameParameter("username") // 아이디 파라미터명 설정
-     *             .passwordParameter("password") // 패스워스 파라미터명 설정
-     *             .loginProcessingUrl("/login") // 로그인 Form Action Url
-     *             .successHandler(loginSuccessHandler) // 로그인 성공 후 핸들러
-     *             .failureHandler(loginFailureHandler) // 로그인 실패 후 핸들러
-     * </pre>
-     */
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -62,5 +50,59 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //.successHandler(loginSuccessHandler)
                 //.failureHandler(loginFailureHandler)
                 .permitAll();
+
+        //스프링 시큐리티는 post 방식으로 logout 지원
+        http
+                .logout() // 로그아웃 처리
+                .logoutUrl("/logout") // 로그아웃 처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동 URL
+                .deleteCookies("JSESSIONID" , "remember-me") // 로그아웃 후 쿠키 삭제
+                .addLogoutHandler(new LogoutHandler() { // 로그아웃 핸들러
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() { // 로그아웃 성공 후 핸들러
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
+                .tokenValiditySeconds(3600) // default 는 14일
+                .alwaysRemember(true) // remember-me 기능이 활성화되지 않아도 항상 실행 유무
+                .userDetailsService(userDetailsService);
+
+        http
+                .sessionManagement() // 세션 관리 기능이 작동
+                .sessionFixation().changeSessionId() // 따로 작성하지 않아도 기본값
+                // changeSessionId(): 세션 아이디 변경, none(): 세션 고정 보호 미설정, migrateSession(): 서블릿 3.1 이하에서 changeSessionId() 작동하도록, newSession(): 새로운 세션 생성
+                .invalidSessionUrl("/invalid") // 세션이 유효하지 않을 경우 이동할 페이지
+                .maximumSessions(1) // 최대 허용 가능 세션 수(-1: 무제한 로그인 세션 허용)
+                .expiredUrl("/expired") // 세션이 만료될 경우 이동할 페이지
+                .maxSessionsPreventsLogin(false)// 동시 로그인 차단함 (false: 기존 세션 만료(default)
+        ;
+    }
+
+    /**
+     * 세션 정책 설정
+     */
+    private void sessionManagement(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                ;
+        /**
+         * SessionCreationPolicy.IF_REQUIRED    : 스프링 시큐리티가 필요시 세션 생성 (default)
+         * SessionCreationPolicy.ALWAYS         : 스프링 시큐리티가 항상 세션 생성
+         * SessionCreationPolicy.NEVER          : 스프링 시큐리티가 생성하지 않지만 이미 존재하면 사용
+         * SessionCreationPolicy.STATELESS      : 스프링 시큐리티가 생성하지 않고 존재해도 사용하지 않음 (JWT)
+         */
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,6 +1,7 @@
 package io.security.basicsecurity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
